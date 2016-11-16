9037cece12325de1d75e5bc2a90e44f37e0a7c4d
Selby <selby@realrider.com>
2016-11-16 12:56:13 +0000

    Full project commit with .gitignore

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(NavMeshAgent))]
+public class Enemy : LivingEntity {
+
+    public enum State {Idle, Chasing, Attacking};
+    State currentState;
+
+    Color originalColour;
+
+    NavMeshAgent pathfinder;
+    Transform target;
+    LivingEntity targetEntity;
+    Material skinMaterial;
+
+    float attackDistanceThreshold = 1.5f;
+    float timeBetweenAttacks = 1;
+    float damage = 1;
+
+    float nextAttackTime;
+
+    bool hasTarget;
+
+	protected override void Start () {
+        base.Start();
+        pathfinder = GetComponent<NavMeshAgent>();
+        skinMaterial = GetComponent<Renderer>().material;
+        originalColour = skinMaterial.color;
+
+        if (GameObject.FindGameObjectWithTag("Player") != null) {
+            currentState = State.Chasing;
+            hasTarget = true;
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+            targetEntity = target.GetComponent<LivingEntity>();
+            targetEntity.OnDeath += OnTargetDeath;
+
+            StartCoroutine(UpdatePath());
+        }
+    }
+
+    void OnTargetDeath() {
+        hasTarget = false;
+        currentState = State.Idle;
+    }
+	
+	void Update () {
+        if (hasTarget) {
+            if (Time.time > nextAttackTime) {
+                float sqrDstToTarget = (target.position - transform.position).sqrMagnitude;
+
+                if (sqrDstToTarget < Mathf.Pow(attackDistanceThreshold, 2)) {
+                    nextAttackTime = Time.time + timeBetweenAttacks;
+                    StartCoroutine(Attack());
+                }
+            }
+        }
+    }
+
+    IEnumerator Attack() {
+        currentState = State.Attacking;
+        pathfinder.enabled = false;
+
+        Vector3 originalPosition = transform.position;
+        Vector3 attackPosition = target.position;
+
+        float attackSpeed = 3;
+        float percent = 0;
+
+        skinMaterial.color = Color.red;
+        bool hasAppliedDamage = false;
+
+        while (percent <= 1){
+            if (percent >= 0.5f && !hasAppliedDamage) {
+                hasAppliedDamage = true;
+                targetEntity.TakeDamage(damage);
+            }
+            percent += Time.deltaTime * attackSpeed;
+            float interpolation = (-Mathf.Pow(percent, 2) + percent) *4;
+            transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolation);
+
+            yield return null;
+        }
+
+        skinMaterial.color = originalColour;
+        currentState = State.Chasing;
+        pathfinder.enabled = true;
+    }
+
+    IEnumerator UpdatePath() {
+        float refreshRate = 0.25f;
+
+        while (hasTarget) {
+            if (currentState == State.Chasing) {
+                Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+                if (!dead)
+                {
+                    pathfinder.SetDestination(targetPosition);
+                }
+            }
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+}
